4a4c8e8acee0289c9ad30a49f7f953fdf8ab42d2
danbin <dan.tranng@gmail.com>
2024-03-22 12:45:03 -0400

    Testing out the "commit" and just added a whitespace

--- src/main/java/de/dennisguse/opentracks/AboutActivity.java
+++ src/main/java/de/dennisguse/opentracks/AboutActivity.java
@@ -17,6 +17,7 @@ public class AboutActivity extends AbstractActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         setTitle(getString(R.string.about_preference_title));
 
         viewBinding.aboutTextDescription.setText(getString(R.string.about_description));
